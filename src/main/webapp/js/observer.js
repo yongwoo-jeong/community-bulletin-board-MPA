179e6a64fa8b310984b64644bb97026a9cb60c7c
yongwoojeong <jywoo9@gmail.com>
2023-03-06 21:35:45 +0900

    Refactored formValidator.js

--- src/main/webapp/js/observer.js
+++ src/main/webapp/js/observer.js
@@ -1,14 +1,56 @@
+/**
+ * 유저 입력값 검증을 위한 클래스
+ */
 class FormValidator {
   constructor(element, regex) {
+    /**
+     * 타겟 필드
+     */
     this.element = element;
+    /**
+     * 정규식
+     */
     this.regex = regex;
+    /**
+     * 검증 여부
+     * @type {boolean} default false
+     */
     this.validation = false;
+    /**
+     * 경고 문구 여부
+     * @type {boolean} default false
+     */
+    this.isWarningOn = false;
     this.element.addEventListener('change', (event) => {
       this.validate(event.target.value);
     });
   }
 
+  /**
+   * 입력값 검증 메서드
+   * @param targetValue
+   */
   validate(targetValue) {
     this.validation = this.regex.test(targetValue);
+    this.updateWarning();
+  }
+
+  /**
+   * 경고문구 업데이트 메서드
+   */
+  updateWarning(){
+    const targetNode = this.element.parentNode;
+    if (!this.isWarningOn && !this.validation){
+      const newElement = document.createElement("span");
+      newElement.innerText = '형식 불일치';
+      newElement.style.color = 'red';
+      targetNode.appendChild(newElement);
+      this.isWarningOn = true;
+    } else if(this.isWarningOn && this.validation){
+      targetNode.querySelector("span").remove();
+      this.isWarningOn = false;
+    }
   }
 }
+
+export default FormValidator;
--- src/main/webapp/js/signUp.js
+++ src/main/webapp/js/signUp.js
@@ -1,4 +1,4 @@
-import formValidation from "./formValidation.js"
+import formValidator from "./observer.js"
 
 // input 엘리먼트 쿼리셀렉트
 const accountInput = document.querySelector('input[name="account"]');
@@ -9,67 +9,19 @@ const emailInput = document.querySelector('input[name="email"]');
 const submitButton = document.querySelector('input[type="submit"]');
 
 // 폼 검증 클래스 인스턴스 생성
-let accountValid = new formValidation(accountInput,/^[a-z0-9]{5,9}$/);
-let passwordValid = new formValidation(passwordInput,  /^[a-z0-9`~!@#$%^&*()-_=+]{6,15}$/);
-let passwordConfirmValid = new formValidation(passwordConfirmInput, /^[a-z0-9`~!@#$%^&*()-_=+]{6,15}$/);
-let nameValid = new formValidation(nameInput, /^[ㄱ-ㅎ|가-힣]{3,4}$/);
-let emailValid = new formValidation(emailInput, /^[-0-9A-Za-z!#$%&'*+/=?^_`{|}~.]+@[-0-9A-Za-z!#$%&'*+/=?^_`{|}~]+[.][0-9A-Za-z]/);
-
-// submit 클릭 시 최종 검증 여부 조회를 위한 불린값 선언
-let isAccountValid = false;
-let isPasswordValid = false;
-let isPasswordConfirmValid = false;
-let isNameValid = false;
-let isEmailValid = false;
-
-
-/**
- * 아이디 인풋 이벤트핸들러
- * @param event
- */
-function onAccountChange(event){
-  isAccountValid = accountValid.isValidate(event.target.value);
-}
-
-/**
- * 비밀번호 이벤트핸들러
- * @param event
- */
-function onPasswordChange(event){
-  isPasswordValid = passwordValid.isValidate(event.target.value);
-}
-
-/**
- * 비밀번호 확인 이벤트핸들러
- */
-function onPasswordConfirmChange(event){
-  isPasswordConfirmValid = passwordConfirmValid.isValidate(event.target.value);
-}
-
-/**
- * 이름 이벤트핸들러
- * @param event
- */
-function onNameChange(event){
-  isNameValid = nameValid.isValidate(event.target.value);
-}
-
-/**
- * 이메일 이벤트핸들러
- * @param event
- */
-function onEmailChange(event){
-  isEmailValid = emailValid.isValidate(event.target.value);
-}
+let accountValid = new formValidator(accountInput,/^[a-z0-9]{5,9}$/);
+let passwordValid = new formValidator(passwordInput,  /^[a-z0-9`~!@#$%^&*()-_=+]{6,15}$/);
+let passwordConfirmValid = new formValidator(passwordConfirmInput, /^[a-z0-9`~!@#$%^&*()-_=+]{6,15}$/);
+let nameValid = new formValidator(nameInput, /^[ㄱ-ㅎ|가-힣]{3,4}$/);
+let emailValid = new formValidator(emailInput, /^[-0-9A-Za-z!#$%&'*+/=?^_`{|}~.]+@[-0-9A-Za-z!#$%&'*+/=?^_`{|}~]+[.][0-9A-Za-z]/);
 
 /**
  * 제출 버튼 클릭시 각 항목 검증 여부 가져와서 확인 후 처리.
- * 비밀번호 일치여부도 이때 확인
- * @param event
+ * 비밀번호 일치여부도 확인
+ * @param event 이벤트 객체
  */
 function onSubmitClick(event){
-  let allInputValidList = [isAccountValid, isPasswordValid, isPasswordConfirmValid, isNameValid, isEmailValid];
-  console.log(allInputValidList);
+  let allInputValidList = [accountValid.validation, passwordValid.validation, passwordConfirmValid.validation, nameValid.validation, emailValid.validation];
   for (let valid of allInputValidList){
     if (valid === false){
       alert("정보가 올바르지 않습니다.");
@@ -83,11 +35,5 @@ function onSubmitClick(event){
   }
 }
 
-// 각 input 이벤트리스너 등록
-accountInput.addEventListener("change",onAccountChange);
-passwordInput.addEventListener("change", onPasswordChange);
-passwordConfirmInput.addEventListener("change", onPasswordConfirmChange);
-nameInput.addEventListener("change", onNameChange);
-emailInput.addEventListener("change", onEmailChange);
 submitButton.addEventListener("click", onSubmitClick);
 
